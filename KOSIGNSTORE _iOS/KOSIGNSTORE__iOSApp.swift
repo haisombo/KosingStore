6692db63fae09878623329468f1013e08bec805e
haisombo <haisombo20@gmail.com>
2024-04-30 13:18:14 +0700

    Initial Commit

---
+++ KOSIGNSTORE _iOS/KOSIGNSTORE__iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  KOSIGNSTORE__iOSApp.swift
+//  KOSIGNSTORE _iOS
+//
+//  Created by Hai Sombo on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct KOSIGNSTORE__iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
